da11933bae7bc1f9d87548a64f91bc3f9a3b397a
Raunak-3 <108402219+Raunak-3@users.noreply.github.com>
2024-08-02 22:01:31 +0530

    Update trainRoutes.js

--- Backend/routes/trainRoutes.js
+++ Backend/routes/trainRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const trainController = require('../controllers/trainController');
-const { verifyToken } = require('../middlewares/authMiddleware');
+// const { verifyToken } = require('../middlewares/authMiddleware');
 
 // Assuming verifyToken middleware is necessary for this route
 router.get('/availability', trainController.getTrainAvailability);
 
 // module.exports = router;
 
-router.post('/create',  verifyToken, trainController.createTrain);
-router.put('/update', verifyToken, trainController.updateTrain);
+router.post('/create',trainController.createTrain);
+router.put('/update', trainController.updateTrain);
+router.get('/:id', trainController.getTrainById);
+
 
 module.exports = router;
 
